7a694069ffc289a5dd320b42128991b7439e0dce
Shashank Kaul <kaulshashank96@gmail.com>
2019-11-30 23:06:41 +0530

    [refactor]

--- routes/magnet.js
+++ routes/magnet.js
@@ -3,47 +3,59 @@ const router = express.Router();
 
 const db = require('../modules/db');
 
-const createMagnet = (uri, title, baseUrl, ctx) => {
-	const key = Math.random().toString(36).slice(4);
-	if (!uri.match(/magnet:\?xt=urn:.*/i)) {
-		console.log(`[ERR!] Invalid request ${uri}`);
-		ctx.res.status(400);
-		ctx.res.json({
-			err: 'Invalid magnet URI'
-		});
-		return;
-	}
+const handleSuccess = ({ res, baseUrl, key }) => res.json({ status: 'OK', uri: baseUrl + '/' + key })
 
-	const value = title ? uri + '@@title@@' + title : uri;
-
-	db.put(key, value)
-		.then(() => ctx.res.json({
-			status: 'OK',
-			uri: baseUrl + '/' + key
-		}))
-		.catch(e => {
+const handleError = ({ res, uri }, err) => {
+	switch (err.message) {
+		case "ERR_NO_URI": {
+			console.log(`[ERR!] Invalid request ${uri}`);
+			return res.status(400).json({
+				err: 'No magnet URI found'
+			});
+		}
+		case "ERR_INVALID_URI": {
+			console.log(`[ERR!] Invalid request ${uri}`);
+			return res.status(400).json({
+				err: 'Invalid magnet URI'
+			});
+		}
+		default: {
 			console.log(`[ERR!] Occured while creating new shortlink`, e.stack);
-			ctx.res.status(520);
-			ctx.res.json({
+			return res.status(520).json({
 				err: 'Unknown err! Please try again'
 			});
-		})
+		}
+	}
 }
 
-router.get('/:body', (req, res, next) => {
+const createMagnet = (uri, title, baseUrl, res) => {
+	if (!uri)
+		return Promise.reject(new Error("ERR_NO_URI"));
+
+	if (!uri.match(/magnet:\?xt=urn:.*/i))
+		return Promise.reject(new Error("ERR_INVALID_URI"));
+
+	const key = Math.random().toString(36).slice(4);
+	const value = title ? uri + '@@title@@' + title : uri;
+	return db.put(key, value)
+		.then(() => handleSuccess({ res, baseUrl, key }))
+		.catch(handleError.bind(undefined, { res, uri }));
+}
+
+router.get('/:body', (req, res) => {
 	const body = req.originalUrl.split('/api/')[1];
-	const [ uri, title ] = body.split('@@title@@');
+	const [uri, title] = body.split('@@title@@');
 	const baseUrl = req.protocol + '://' + req.get('host');
 
-	return createMagnet(uri, title, baseUrl, { req, res });
-})
+	return createMagnet(uri, title, baseUrl, res);
+});
 
 /* POST magnet link. */
-router.post('/', (req, res, next) => {
+router.post('/', (req, res) => {
 	const { uri, title } = req.body;
 	const baseUrl = req.protocol + '://' + req.get('host');
 
-	return createMagnet(uri, title, baseUrl, { req, res });
+	return createMagnet(uri, title, baseUrl, res);
 });
 
 module.exports = router;
